b4feb2c4e33cdfe3a6d98722e3f9b58fc996099d
Shrinkhla Rajpoot <shrinkhlarajpoot@gmail.com>
2022-04-07 21:53:07 +0530

    feat filters

--- src/frontend/components/Footer/Footer.jsx
+++ src/frontend/components/Footer/Footer.jsx
@@ -4,9 +4,9 @@ const Footer = () =>{
         <div className="footer">
         <h5>Made by Shrinkhla</h5>
         <div className="social__links">
-          <i className="fa fa-linkedin" aria-hidden="true"></i>
-          <i className="fa fa-github" aria-hidden="true"></i>
-          <i className="fa fa-twitter" aria-hidden="true"></i>
+         <a href="https://www.linkedin.com/in/shrinkhla-rajpoot-aaa74b1a7/"> <i className="fa fa-linkedin" aria-hidden="true"></i></a>
+        <a href="https://github.com/Shrinkhlarajpoot" > <i className="fa fa-github" aria-hidden="true"></i></a>
+         <a href="https://twitter.com/ShrinkhlaR"> <i className="fa fa-twitter" aria-hidden="true"></i></a>
           </div> 
         </div>
     )
